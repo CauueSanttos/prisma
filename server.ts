dce55b6963db5ce58b50b7111dc33f71a80c4bef
Cauê <cauedossantossilva@hotmail.com>
2022-05-14 07:05:24 -0300

    tests prisma client using node

---
+++ server.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.courses.create({
+    data: {
+      duration: 200,
+      name: "Curso de Java",
+      description: "Curso de Java bom",
+    },
+  });
+
+  console.log(result);
+}
+
+main();
